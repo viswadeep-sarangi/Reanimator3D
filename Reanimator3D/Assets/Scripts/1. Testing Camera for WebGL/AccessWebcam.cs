f21fd65070d784c541cf09fbcb5ec92e1e3271a2
Viswadeep Sarangi <v.sarangi@york.ac.uk>
2021-01-08 23:13:58 +0000

    First commit. Let's see if this works

---
+++ Reanimator3D/Assets/Scripts/1. Testing Camera for WebGL/AccessWebcam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AccessWebcam : MonoBehaviour
+{
+    public RawImage webcamRawImage;
+    public TextMeshProUGUI Msgs;
+
+    private WebCamTexture webCamTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Msgs.text = "Starting please wait...";
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length == 0)
+        {
+            Msgs.text = "No cameras found!";
+            return;
+        }
+
+        webCamTexture = new WebCamTexture(devices[0].name);
+        webcamRawImage.texture = webCamTexture;
+        webCamTexture.Play();
+
+        Msgs.text = "Playing webcam texture now...";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
