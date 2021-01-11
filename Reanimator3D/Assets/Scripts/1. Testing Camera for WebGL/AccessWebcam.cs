3e743daa09ad2af0ce7d2a11bd5319a2ba1ce0a8
Viswadeep Sarangi <v.sarangi@york.ac.uk>
2021-01-11 13:40:38 +0000

    added aspect ratio fitter to resize camera properly

--- Reanimator3D/Assets/Scripts/1. Testing Camera for WebGL/AccessWebcam.cs
+++ Reanimator3D/Assets/Scripts/1. Testing Camera for WebGL/AccessWebcam.cs
@@ -26,6 +26,11 @@ public class AccessWebcam : MonoBehaviour
         webcamRawImage.texture = webCamTexture;
         webCamTexture.Play();
 
+        var arf = webcamRawImage.gameObject.GetComponent<AspectRatioFitter>();
+        //arf.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+        arf.aspectRatio = webCamTexture.width*1.0f / webCamTexture.height;
+        Debug.Log(string.Format("Webcam width={0}, height={1}, aspect ratio={2}", webCamTexture.width, webCamTexture.height, arf.aspectRatio));
+
         Msgs.text = "Playing webcam texture now...";
     }
 
